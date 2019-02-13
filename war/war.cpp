4d330b17bef6e4736b3cda887c02c1744ffac4db
jsy15 <jsy15@zips.uakron.edu>
2019-02-13 00:17:45 -0500

    Fixed CMakeLists.txt and corrected war to play the game

--- war/war.cpp
+++ war/war.cpp
@@ -1,16 +1,9 @@
-#include "card.hpp"
-#include "deck.hpp"
-
-
-
-
+#include "game.hpp"
 
 int main(){
     
     //Creates the Deck object and starts the game of war
-    Deck deck;
-    deck.playWar();
+    Game game;
+    game.play_game();  
     
-
-    return 0;
 }
