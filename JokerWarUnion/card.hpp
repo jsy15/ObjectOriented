7d0eba436b067302f5e552fd93ffe5942e73bf16
Jacob Yelling <jsy15@zips.uakron.edu>
2019-02-20 01:40:32 -0500

    Makes changes to card.hpp for tagged union

--- JokerWarUnion/card.hpp
+++ JokerWarUnion/card.hpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <iosfwd>
+#include <cassert>
 
 //Enums for the suit and ranks
 enum Suit {
@@ -28,6 +29,11 @@ enum Rank {
   King
 };
 
+enum Color {
+   Black,
+   Red
+};
+
 class StandardCard {
    public:
 
@@ -49,6 +55,95 @@ class StandardCard {
    private:
       unsigned char bits;
 
+};
+
+class JokerCard {
+   public:
+   JokerCard(Color c)
+      : color(c)
+      { }
+
+   Color get_color() const { return color; }
+
+   private:
+      Color color;
+};
+
+union PlayingCardData {
+   PlayingCardData(Rank r, Suit s)
+      : sc(r, s)
+      { }
+
+   PlayingCardData(Color c)
+      : jc(c)
+      { }
+
+      StandardCard sc;
+      JokerCard jc;
+};
+
+enum PlayingCardKind {
+   Standard,
+   Joker
+};
+
+class PlayingCard {
+   public:
+      PlayingCard(Rank r, Suit s)
+         : tag(Standard), data(r,s)
+         { }
+
+      PlayingCard(Color c)
+         : tag(Joker), data(c)
+         { }
+
+      bool is_joker() const {
+         if(tag == Joker){
+            return true;
+         }
+         else{
+            return false;
+         }         
+      };
+
+      bool is_standard() const{
+         if(tag == Standard){
+            return true;
+         }
+         else{
+            return false;
+         }
+         
+      };
+
+      StandardCard getStandardCard() const {
+         assert(is_standard());
+         return data.sc;
+      };
+
+      JokerCard getJokerCard() const {
+         assert(is_joker());
+         return data.jc;
+      }
+
+      Rank get_rank() const{
+         assert(is_standard());
+         return data.sc.get_rank();
+      }
+
+      Suit get_suit() const {
+         assert(is_standard());
+         return data.sc.get_suit();
+      };
+
+      Color get_color() const{
+         assert(is_joker());
+         return data.jc.get_color();
+      }
+
+   private:
+      PlayingCardKind tag;
+      PlayingCardData data;
 };
 
 //Overloading the ostream operator to fix the enums for output
